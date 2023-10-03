7daf21798baea3aefae846398146a9072d58e9e6
obidavids <obiudemba8@gmail.com>
2023-10-02 20:35:17 -0400

    add hellow world changes

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,27 @@
+# Hello World App
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hello World</h1>"
+
+
+@app.route("/second")
+def second():
+    return "<h2>This is the second page</h2>"
+
+
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage of third page"
+
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"The id of this page is {id}"
+
+# Run the flask app
+app.run(debug=False)

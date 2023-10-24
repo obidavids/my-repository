5483d62ad96fcc794e67459759f4ffd2d02f034b
obidavids <obiudemba8@gmail.com>
2023-10-23 21:35:16 -0400

    fix index

--- 101-kittens-carousel-static-website-ec2/setup.sh
+++ 101-kittens-carousel-static-website-ec2/setup.sh
@@ -1,3 +1,17 @@
+#!/bin/bash -x
+
+# update system and install apache
 yum update -y
 yum install httpd -y
+
+#change to the directory and download content
 cd /var/www/html
+FOLDER="https://raw.githubusercontent.com/obidavids/my-repository/main/101-kittens-carousel-static-website-ec2/static-web/"
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+
+# start and enbale httpd
+systemctl start httpd
+systemctl enable httpd
